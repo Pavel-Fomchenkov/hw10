eefd9eea24c23094c127b07475c26798540f59da
Pavel-Fomchenkov <donotwritemail@gmail.com>
2023-08-28 22:39:27 +0300

    hw10 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+
+    private static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName +" "+ firstName +" "+ middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    private static void task2() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName +" "+ firstName +" "+ middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+
+    private static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника - " + fullName.replace('ё', 'е'));
+    }
+
 }
